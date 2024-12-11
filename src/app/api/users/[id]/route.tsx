1662a4c606c05c46aa0dd6e2bc9fd3c9d9ea01a4
yuan <2284810742@qq.com>
2024-12-11 20:36:59 +0800

    1

--- src/app/api/users/[id]/route.tsx
+++ src/app/api/users/[id]/route.tsx
@@ -1,20 +1,20 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 // GET 方法，用于处理动态路由的请求
-export function GET(
-  request: NextRequest,
-  { params }: { params: { id: string } } // 明确指定 params 类型为 { id: string }
-) {
-  const { id } = params;
+// export function GET(
+//   request: NextRequest,
+//   { params }: { params: { id: string } } // 明确指定 params 类型为 { id: string }
+// ) {
+//   const { id } = params;
 
-  // 将字符串类型的 id 转换为数字
-  const numId = parseInt(id);
+//   // 将字符串类型的 id 转换为数字
+//   const numId = parseInt(id);
 
-  // 检查转换后的 id 是否有效
-  if (numId > 10 || isNaN(numId)) {
-    return NextResponse.json({ error: "User not found" }, { status: 404 });
-  }
+//   // 检查转换后的 id 是否有效
+//   if (numId > 10 || isNaN(numId)) {
+//     return NextResponse.json({ error: "User not found" }, { status: 404 });
+//   }
 
-  // 返回成功响应
-  return NextResponse.json({ id: numId, name: "Castamere" });
-}
+//   // 返回成功响应
+//   return NextResponse.json({ id: numId, name: "Castamere" });
+// }
